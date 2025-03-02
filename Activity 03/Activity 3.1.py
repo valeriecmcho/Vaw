9e34fb8a9fbdd9250fce634e469b7c7029ca7405
Kirkiee <justinkirkendozo@gmail.com>
2025-03-02 22:24:41 +0800

    Initial Commit

--- Activity 03/Activity 3.1.py
+++ Activity 03/Activity 3.1.py
@@ -3,14 +3,18 @@ first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 age = input("Enter your age: ")
 
+
 #This concatenates the first name and last name into a full name
 full_name = first_name + " " + last_name
 
-#This slices the first name 
+
+#This slices the first name
 sliced_name = first_name[:3]  # Extracting the first three characters
+
 
 #This creates a greeting message
 greeting_message = f"Hello, {sliced_name}! Welcome. You are {age} years old."
+
 
 #This prints all the results
 print("\nFull Name:", full_name)
--- Activity 03/StringManip4.py
+++
@@ -1,9 +0,0 @@
-# This opens the file in read mode
-
-with open("students.txt", "r") as file:
-        # This reads the contents of the file
-        student_data = file.read()
-    
-    # This displays the student information
-print("\nReading Student Information:\n")
-print(student_data)
